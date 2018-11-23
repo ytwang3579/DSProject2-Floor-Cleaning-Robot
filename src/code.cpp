3a068ba105c1ecdd053581a13f2e1011d359b2f9
ytwang <ytwang3579@gmail.com>
2018-11-24 00:02:59 +0800

    correct version 1

--- src/code.cpp
+++ src/code.cpp
@@ -18,14 +18,16 @@ ofstream outfile;
 //global variable
 int M, N, B, ri, rj;
 vector<char> map;
-vector<pair<pair<int, int>, int>> _dest; //_destination: storing shortest path from 'R'
+vector<pair<pair<int, int>, int>> dest; //_destination: storing shortest path from 'R'
 enum Action{UP, DOWN, LEFT, RIGHT, ERROR};
 vector<pair<int, int>> stk; //reversing the step when gothrough()
+queue<pair<int, int>> ans;
+vector<int> bfs_r, bfs_up, bfs_down, bfs_left, bfs_right;
 
 vector<int> BFS(int i, int j, vector<char> tmap, bool flag)
 {
 
-    vector<int> bfs(M*N,-1);
+    vector<int> bfs(M*N,B);
     if(i<0 || j<0 || i>=M || j>=N) return bfs;
 
     queue<pair<int, int>> q;
@@ -45,25 +47,25 @@ vector<int> BFS(int i, int j, vector<char> tmap, bool flag)
 
         if(i>0) if(tmap[T_UP]=='0') {
             q.push(make_pair(i-1, j));
-            _dest.push_back(make_pair(q.back(), cnt));
+            if(flag) dest.push_back(make_pair(q.back(), cnt));
             bfs[T_UP] = cnt;
             tmap[T_UP] = 'D';
         }
         if(i<M-1) if(tmap[T_DOWN]=='0'){
             q.push(make_pair(i+1, j));
-            _dest.push_back(make_pair(q.back(), cnt));
+            if(flag) dest.push_back(make_pair(q.back(), cnt));
             bfs[T_DOWN] = cnt;
             tmap[T_DOWN] = 'D';
         }
         if(j>0) if(tmap[T_LEFT]=='0'){
             q.push(make_pair(i, j-1));
-            _dest.push_back(make_pair(q.back(), cnt));
+            if(flag) dest.push_back(make_pair(q.back(), cnt));
             bfs[T_LEFT] = cnt;
             tmap[T_LEFT] = 'D';
         }
         if(j<N-1) if(tmap[T_RIGHT]=='0'){
             q.push(make_pair(i, j+1));
-            _dest.push_back(make_pair(q.back(), cnt));
+            if(flag) dest.push_back(make_pair(q.back(), cnt));
             bfs[T_RIGHT] = cnt;
             tmap[T_RIGHT] = 'D';
         }
@@ -73,22 +75,118 @@ vector<int> BFS(int i, int j, vector<char> tmap, bool flag)
     }
     return bfs;
 }
-
-void gothrough(const int i, const int j, const Action action)
+void go_a_step(int& i, int& j, const vector<int>& bfs)
+{
+    if(i>0) if(map[T_UP]=='0') if(bfs[T_UP]==bfs[TARGET]-1){
+        i--; return;
+    }
+    if(i<M-1) if(map[T_DOWN]=='0') if(bfs[T_DOWN]==bfs[TARGET]-1){
+        i++; return;
+    }
+    if(j>0) if(map[T_LEFT]=='0') if(bfs[T_LEFT]==bfs[TARGET]-1){
+        j--; return;
+    }
+    if(j<N-1) if(map[T_RIGHT]=='0') if(bfs[T_RIGHT]==bfs[TARGET]-1){
+        j++; return;
+    }
+    if(i>0) if(bfs[T_UP]==bfs[TARGET]-1 || bfs[T_UP]==B){
+        i--; return;
+    }
+    if(i<M-1) if(bfs[T_DOWN]==bfs[TARGET]-1 || bfs[T_DOWN]==B){
+        i++; return;
+    }
+    if(j>0) if(bfs[T_LEFT]==bfs[TARGET]-1 || bfs[T_LEFT]==B){
+        j--; return;
+    }
+    if(j<N-1) if(bfs[T_RIGHT]==bfs[TARGET]-1 || bfs[T_RIGHT]==B){
+        j++; return;
+    }
+}
+void gothrough(int i, int j, const Action action)
 {
     if(action==ERROR){
         throw string("Error when going through path: There is no way for some element(s)\n");
         return;
     }
 
-    switch(action){
-        //TODO: iterate through the destination and change the map value to label visited
+    //cout << action << ' ' << "dest:(" << i << ", " << j << ")\n";
 
+    switch(action){
+        //TODO: iterate through the destination and change the map value to label 'visited'
+        case UP:
+            while(bfs_up[TARGET]<B){
+                map[TARGET] = 'D';
+                stk.push_back(make_pair(i, j));
+                go_a_step(i, j, bfs_up);
+            }
+            while(stk.empty()==false){
+                ans.push(stk.back());
+                stk.pop_back();
+            }
+            i = ans.back().first; j = ans.back().second;
+            while(bfs_up[TARGET]<B){
+                go_a_step(i, j, bfs_up);
+                ans.push(make_pair(i, j));
+                map[TARGET] = 'D';
+            }
+            break;
+        case DOWN:
+            while(bfs_down[TARGET]<B){
+                map[TARGET] = 'D';
+                stk.push_back(make_pair(i, j));
+                go_a_step(i, j, bfs_down);
+            }
+            while(stk.empty()==false){
+                ans.push(stk.back());
+                stk.pop_back();
+            }
+            i = ans.back().first; j = ans.back().second;
+            while(bfs_down[TARGET]<B){
+                go_a_step(i, j, bfs_down);
+                ans.push(make_pair(i, j));
+                map[TARGET] = 'D';
+            }
+            break;
+        case LEFT:
+            while(bfs_left[TARGET]<B){
+                map[TARGET] = 'D';
+                stk.push_back(make_pair(i, j));
+                go_a_step(i, j, bfs_left);
+            }
+            while(stk.empty()==false){
+                ans.push(stk.back());
+                stk.pop_back();
+            }
+            i = ans.back().first; j = ans.back().second;
+            while(bfs_left[TARGET]<B){
+                go_a_step(i, j, bfs_left);
+                ans.push(make_pair(i, j));
+                map[TARGET] = 'D';
+            }
+            break;
+        case RIGHT:
+            while(bfs_right[TARGET]<B){
+                map[TARGET] = 'D';
+                stk.push_back(make_pair(i, j));
+                go_a_step(i, j, bfs_right);
+            }
+            while(stk.empty()==false){
+                ans.push(stk.back());
+                stk.pop_back();
+            }
+            i = ans.back().first; j = ans.back().second;
+            while(bfs_right[TARGET]<B){
+                go_a_step(i, j, bfs_right);
+                ans.push(make_pair(i, j));
+                map[TARGET] = 'D';
+            }
+            break;
     }
+
+    
 }
 
-Action nextaction(const int i, const int j, const vector<int>& bfs_up, 
-                    const vector<int>& bfs_down, const vector<int>& bfs_left, const vector<int>& bfs_right)
+Action nextaction(const int i, const int j)
 {
     Action action = UP;
     int minstep = bfs_up[TARGET];
@@ -105,33 +203,40 @@ Action nextaction(const int i, const int j, const vector<int>& bfs_up,
         action = RIGHT;
     }
     
-    return ((minstep>0) ? action : ERROR);
+    return ((minstep<B) ? action : ERROR);
 }
 
 void cleaning_naive()
 {
 
-    vector<int> bfs = BFS(ri, rj, map, 1);
-    vector<pair<pair<int, int>, int>> dest = _dest;
+    bfs_r = BFS(ri, rj, map, 1);
+    //vector<pair<pair<int, int>, int>> dest = _dest;
     //_dest.clear();
 
-    vector<int> bfs_up = BFS(ri-1, rj, map, 0);
+    bfs_up = BFS(ri-1, rj, map, 0);
     //vector<pair<pair<int, int>, int>> dest_up = _dest;
     //_dest.clear();
 
-    vector<int> bfs_down = BFS(ri+1, rj, map, 0);
+    bfs_down = BFS(ri+1, rj, map, 0);
     //vector<pair<pair<int, int>, int>> dest_down = _dest;
     //_dest.clear();
 
-    vector<int> bfs_left = BFS(ri, rj-1, map, 0);
+    bfs_left = BFS(ri, rj-1, map, 0);
     //vector<pair<pair<int, int>, int>> dest_left = _dest;
     //_dest.clear();
 
-    vector<int> bfs_right = BFS(ri, rj+1, map, 0);
+    bfs_right = BFS(ri, rj+1, map, 0);
     //vector<pair<pair<int, int>, int>> dest_right = _dest;
-    _dest.clear(); _dest.shrink_to_fit();
+    //_dest.clear(); _dest.shrink_to_fit();
 
-/* test BFS
+ /*/test BFS
+    for(int i=0; i<M; i++){
+        for(int j=0; j<N; j++){
+            outfile << bfs_r[TARGET];
+            outfile << ((j==N-1) ? '\n' : ' ');
+        }
+    }
+    outfile << '\n';
     for(int i=0; i<M; i++){
         for(int j=0; j<N; j++){
             outfile << bfs_up[TARGET];
@@ -164,13 +269,15 @@ void cleaning_naive()
     while(dest.empty()==false){
         int i = dest.back().first.first;
         int j = dest.back().first.second;
+
+        //cout << i << ' ' << j << ' ' << dest.back().second << '\n';
         
         if(map[TARGET]!='0'){
             dest.pop_back(); continue;
         }
 
         try{
-            gothrough(i, j, nextaction(i, j, bfs_up, bfs_down, bfs_left, bfs_right));
+            gothrough(i, j, nextaction(i, j));
         } catch(string c){cout << c;}
 
         dest.pop_back();
@@ -220,6 +327,11 @@ int main(int argc, char* argv[])
     }
     
     cleaning_naive();
+    outfile << ans.size() << '\n';
+    while(ans.empty()==false){
+        outfile << ans.front().first << ' ' << ans.front().second << '\n';
+        ans.pop();
+    }
 
     return 0;
 }
